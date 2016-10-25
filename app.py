05cdfe6a6b3c65c482067b367c4959706354c4e8
Dong-Hyun Kim <dhexonian@aol.com>
2016-10-25 01:18:58 -0700

    add flask app

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, url_for, render_template 
+
+from flask_appconfig import AppConfig
+from flask_bootstrap import Bootstrap
+
+app = Flask(__name__)
+Bootstrap(app)
+
+app.debug = True
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+@app.route("/about")
+def about():
+    return render_template('about.html')
+
+@app.route("/present")
+def present():
+    return render_template('present.html')
+
+if __name__ == "__main__":
+    app.run()
+
+
+# def render_sidebar_template(tmpl_name, **kwargs):
+#     (var1, var2, var3) = generate_sidebar_data()
+#     return render_template(tmpl_name, var1=var1, var2=var2, var3=var3, **kwargs)
